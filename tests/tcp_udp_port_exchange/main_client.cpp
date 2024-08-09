f75ac22023b2c5b176ce41283dab483b7fc91ac8
simao <simao_meneses_joao@hotmail.com>
2024-08-09 14:46:18 +0100

    update

--- tests/tcp_udp_port_exchange/main_client.cpp
+++ tests/tcp_udp_port_exchange/main_client.cpp
@@ -20,25 +20,14 @@ int main(int argc, char** argv){
     std::string msg = "UDP OK";
     UDP.send_data(msg.c_str(), msg.size()+1);
 
+    // Receive acknowledgement that UDP packet was received
+    TCP.receive_data(buffer, 100);
+    response = std::string(buffer);
+    std::cout << "Received: " << response << "\n";
 
-
-
-    // int data[5]{1,2,1,2,4};
-    // client.print_current_address();
-    // client.send_data(data, 5*sizeof(int));
-
-    // int data_rec[5];
-    // client.print_current_address();
-    // client.receive_data(data_rec, 5*sizeof(int));
-    // std::cout << "Message received:"; 
-    // for(int i=0; i<5; i++)std::cout << data_rec[i];
-    // std::cout << "\n";
-
-    // usleep(200*1000);
-    // client.print_current_address();
-    // client.send_data(data, 5*sizeof(int));
-    // client.print_current_address();
-    
+    // Tell the server that the client is ready
+    msg = "Client ready";
+    TCP.send_data(msg.c_str(), msg.size()+1);
     
     
     return 0;
--- tests/tcp_udp_port_exchange/main_server.cpp
+++ tests/tcp_udp_port_exchange/main_server.cpp
@@ -12,39 +12,24 @@ int main(int argc, char** argv){
 
     UDPServer UDP(portUDP);
 
+    // Inform the client about the UDP port available
     std::string msg = "OK,PORT="+portUDP;
     connection.send_data(msg.c_str(), msg.size()+1);
     
-
+    // Receive acknowledgement from client. This step may be important for NAT traversal
+    // Server already has an open port for UDP, so hope punching is not required
+    // This step also sets the client's address in the UDP server
     char buffer[100];
     UDP.receive_data(buffer, 100);
     std::cout << "server received: " << std::string(buffer) << "\n";
+    UDP.print_current_address();
+
+    // Acknowledge UDP packet received
+    msg = "UDP packet received";
+    connection.send_data(msg.c_str(), msg.size()+1);
 
 
-
-    // UDPServer server("8448",1);
-
-    // int data[5];
-    // server.print_current_address();
-    // server.receive_data(data, 5*sizeof(int));
-    // std::cout << "Message received:"; 
-    // for(int i=0; i<5; i++) std::cout << data[i];
-    // std::cout << "\n";
-
-
-    // std::cout << "Sending doubled data back\n";
-    // for(int i=0; i<5; i++)data[i] *= 2;
-    // server.print_current_address();
-    // server.send_data(data, 5*sizeof(int));
-    // server.print_current_address();
-
-
-
-    // server.receive_data(data, 5*sizeof(int));
-    // server.print_current_address();
-    // std::cout << "Message received:"; 
-    // for(int i=0; i<5; i++)std::cout << data[i] << " ";
-    // std::cout << "\n";
-
-    // return 0;
+    connection.receive_data(buffer, 100);
+    std::cout << "server received: " << std::string(buffer) << "\n";
+    return 0;
 }
