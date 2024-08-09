7918adffd1bb27ca24e3b351686fc128c02846f0
simao <simao_meneses_joao@hotmail.com>
2024-08-09 12:37:31 +0100

    update

--- tests/udp/main_server.cpp
+++ tests/udp/main_server.cpp
@@ -15,6 +15,15 @@ int main(int argc, char** argv){
     }
     std::cout << "\n";
 
+    server.receive_data(data, 5*sizeof(int));
+    
+    std::cout << "Message:\n"; 
+    for(int i=0; i<5; i++){
+        std::cout << data[i] << " ";
+        data[i] *= 2; // double the data
+    }
+    std::cout << "\n";
+
     std::cout << "Sending doubled data back\n";
     server.send_data(data, 5*sizeof(int));
 
